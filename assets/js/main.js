4f85677c3267957f7dfe28180fb28e860660ec76
Wellysson Rocha <wellysson35@gmail.com>
2023-03-19 19:18:40 -0300

    :data:

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+    <li id="${pokemon.number}" class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -23,10 +23,45 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
+function pegaPokemon(pokemon) {
+    return `
+        <span id="voltar">Clique aqui para voltar</span>
+        <h1 class="name">${pokemon.name}</h1>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+        </div>
+    `
+}
+
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+
+        // Adicione o ouvinte de evento `click` à tag `li`
+        pokemons.forEach(pokemon => {
+            const li = document.getElementById(pokemon.number)
+            li.addEventListener('click', () => {
+                loadPokemonUnic(pokemon)
+            })
+        })
+    })
+}
+
+function loadPokemonUnic(pokemon){
+    auxHtml = pokemonList.innerHTML;
+    pokemonList.innerHTML = pegaPokemon(pokemon)
+
+    const volta = document.getElementById('voltar');
+    volta.addEventListener('click', () => {
+        pokemonList.innerHTML = '';
+        loadPokemonItens(offset, limit)
     })
 }
 
